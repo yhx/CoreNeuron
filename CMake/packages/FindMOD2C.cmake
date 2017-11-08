e50c6d8d589cc48d8b9dfbff229a58d29844e427
Adrien Devresse <adrien.devresse@epfl.ch>
2017-11-08 14:46:39 +0100

    Refactor CoreNeuron / mod2c CMake chain
    - Remove ExternalProject needs, avoid the problems related to it
    - Add a "DISABLE_BUNDLE" mode where external mod2c can be found and use
    - Add mod2c as a `add_subdirectory`
    - Properly propagate mod2c targets to the generators to avoid parallel build dependencies issues
    - Validate the build with NinnnnnnJa (Tim's style) on both viz cluster and BGQ
    - Follow the same pattern for frontend / backend compiler than before
    
    Adev

--- CMake/packages/FindMOD2C.cmake
+++ CMake/packages/FindMOD2C.cmake
@@ -25,28 +25,12 @@
 # THE POSSIBILITY OF SUCH DAMAGE.
 
 
-# Findmod2c
+# FindMOD2C
 # -------------
 #
-# Find mod2c
+# Find MOD2C
 #
-# Find the mod2c Blue Brain HPC utils library
 #
-# Using mod2c:
-#
-# ::
-#
-#   find_package(mod2c REQUIRED)
-#   include_directories(${mod2c_INCLUDE_DIRS})
-#   target_link_libraries(foo ${mod2c_LIBRARIES})
-#
-# This module sets the following variables:
-#
-# ::
-#
-#   mod2c_FOUND - set to true if the library is found
-#   mod2c_INCLUDE_DIRS - list of required include directories
-#   mod2c_LIBRARIES - list of libraries to be linked
 
 #=============================================================================
 # Copyright 2015 Adrien Devresse <adrien.devresse@epfl.ch>
@@ -57,16 +41,30 @@
 
 
 # UNIX paths are standard, no need to write.
-find_program(mod2c_BINARY
+find_program(MOD2C_BINARY
   NAMES mod2c_core
-  PATHS "/usr/bin"
-  )
+)
 
+find_file(MOD2C_MODLUNIT
+    NAMES nrnunits.lib
+    PATH_SUFFIXES "/share" "/usr/share"
+)
 
 # Checks 'REQUIRED', 'QUIET' and versions.
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(mod2c
-  FOUND_VAR mod2c_FOUND
-  REQUIRED_VARS mod2c_BINARY)
+find_package_handle_standard_args(MOD2C
+  FOUND_VAR MOD2C_FOUND
+  REQUIRED_VARS MOD2C_BINARY MOD2C_MODLUNIT)
+
+if(MOD2C_FOUND)
+    message(STATUS "Found and use nrnunits.lib under ${MOD2C_MODLUNIT} ")
+else()
+    message(STATUS "nrnunits.lib non found")
+endif()
+
+
+mark_as_advanced(MOD2C_FOUND MOD2C_BINARY MOD2C_MODLUNIT)
+
+
   

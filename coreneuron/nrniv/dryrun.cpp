39d7700031d07e1413c847ab587b6cac09743c04
fouriaux <jeremy.fouriaux@epfl.ch>
2018-03-12 13:45:03 +0100

    refactoring and clean up of dryrun

--- coreneuron/nrniv/dryrun.cpp
+++ coreneuron/nrniv/dryrun.cpp
@@ -31,57 +31,60 @@
 #include <sys/time.h>
 #include <stdlib.h>
 #include <fstream>
+#include <iostream>
+#include <vector>
 static inline double elapsed_in_micro_sec (timeval& start, timeval& stop) {
     return (stop.tv_sec - start.tv_sec)*10.0e6 + (stop.tv_usec - start.tv_usec);
 }
+
+static std::vector<double> channel_measures;
+static std::vector<int>    channel_counters;
+
+static inline void profile_mechanism_function (mod_f_t f, NrnThread* cur_nt, NrnThreadMembList* tml) {
+    timeval start, stop;
+    gettimeofday(&start,NULL);
+    (*f)(cur_nt, tml->ml, tml->index);
+    gettimeofday(&stop,NULL);
+    channel_measures[tml->index] += elapsed_in_micro_sec (start, stop);
+    channel_counters[tml->index] +=1;
+
+}
 extern "C" int execute_dryrun () {
     NrnThreadMembList* tml;
-    timeval start, stop;
     double elapsed;
     std::fstream mcomplex_file;
-    double* channel_measures  = (double*) calloc (get_nb_mechs_avail(),sizeof(double));
-    int* channel_counters     = (int*) calloc (get_nb_mechs_avail(), sizeof(int));
+    channel_measures.clear();
+    channel_counters.clear();
+    channel_measures.resize(get_nb_mechs_avail()); 
+    channel_counters.resize(get_nb_mechs_avail());
     for (int t_idx = 0 ; t_idx < nrn_nthread; t_idx++) {
         NrnThread* cur_nt = &nrn_threads[t_idx];
         for (tml = cur_nt->tml; tml; tml = tml->next) {
             if (memb_func[tml->index].state) {
-                mod_f_t f = memb_func[tml->index].state;
-                gettimeofday(&start,NULL);
-                (*f)(cur_nt, tml->ml, tml->index);
-                gettimeofday(&stop,NULL);
-                channel_measures[tml->index] += elapsed_in_micro_sec (start, stop);
-                channel_counters[tml->index] +=1;
+                profile_mechanism_function(memb_func[tml->index].state, cur_nt, tml);
             }
         }
         for (tml = cur_nt->tml; tml; tml = tml->next) {
             if (memb_func[tml->index].current) {
-                mod_f_t f = memb_func[tml->index].current;
-                gettimeofday(&start,NULL);
-                (*f)(cur_nt, tml->ml, tml->index);
-                gettimeofday(&stop,NULL);
-                channel_measures[tml->index] += elapsed_in_micro_sec (start, stop);
-                channel_counters[tml->index] +=1;
+                profile_mechanism_function(memb_func[tml->index].current, cur_nt, tml);
             }
         }
         for (tml = cur_nt->tml; tml; tml = tml->next) {
             if (memb_func[tml->index].jacob) {
-                mod_f_t f = memb_func[tml->index].jacob;
-                gettimeofday(&start,NULL);
-                (*f)(cur_nt, tml->ml, tml->index);
-                gettimeofday(&stop,NULL);
-                channel_measures[tml->index] += elapsed_in_micro_sec (start, stop);
-                channel_counters[tml->index] +=1;
+                profile_mechanism_function(memb_func[tml->index].jacob, cur_nt, tml);
             }
         }
     }
-    for (int i = 0 ; i < get_nb_mechs_avail(); i++) {
+    for (int i = 0 ; i < channel_measures.size(); i++) {
         if (channel_counters[i]) {
-            channel_measures[i] /= channel_counters[i];
+            channel_measures[i] /= (double) channel_counters[i];
         }
     }
     mcomplex_file.open("mcomplex.dat", std::ios_base::out);
-    for (int i = 0 ; i < get_nb_mechs_avail(); i++) {
-        mcomplex_file << channel_measures[i] << " " << memb_func[i].sym << "\n";
+    for (int i = 2 ; i < channel_measures.size(); i++) {
+        if (memb_func[i].sym) {
+            mcomplex_file << channel_measures[i] << " " << memb_func[i].sym << std::endl;
+        }
     }
     mcomplex_file.close();
     return 0;

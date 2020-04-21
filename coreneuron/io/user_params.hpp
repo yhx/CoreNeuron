6c01a99e9a032220ce556b532ecc0b64a64f5747
Nicolas Cornu <nicolas.cornu@epfl.ch>
2020-04-21 10:01:06 +0200

    Apply suggestions from code review
    Co-Authored-By: Pramod Kumbhar <pramod.kumbhar@epfl.ch>

--- coreneuron/io/user_params.hpp
+++ coreneuron/io/user_params.hpp
@@ -10,10 +10,12 @@ struct UserParams {
     /// Array of duplicate indices. Normally, with nrn_setup_multiple=1,
     //   they are ngroup values of 0.
     int* imult;
+    /// pah to dataset file
     const char* path;
+    ///dataset  path from where simulation is being restored
     const char* restore_path;
     FileHandler* file_reader;
+    /// true if byte swapping should be done (little endian <-> big endian) 
     bool byte_swap;
 };
 }  // namespace coreneuron
-

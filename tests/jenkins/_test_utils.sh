f382ce43d9b3b54f230516eff6d40628d641c22a
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-04-23 18:26:00 +0200

    Added nrntraub test

---
+++ tests/jenkins/_test_utils.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bb5_run() (
+    set +x -e
+    # default partition is interactive. during night use production
+    hour=`date +%H`
+    weekday=`date +%u`
+    if [ "$hour" -ge "19" ] || [ "$hour" -lt "8" ] || [ $weekday -gt 5 ]; then export SALLOC_PARTITION="prod"; fi
+
+    N=${N:-1}
+    if [ -n "$n" ]; then
+        SALLOC_OPTS="$SALLOC_OPTS --ntasks-per-node=$n --exclusive --mem=0"
+    else
+        SALLOC_OPTS="$SALLOC_OPTS --ntasks-per-node=36 --exclusive --mem=0"
+    fi
+
+    cmd_base="time salloc -N$N $SALLOC_OPTS -Aproj16 --hint=compute_bound -Ccpu|nvme --time 1:00:00 srun dplace "
+    echo "$cmd_base $@"
+    $cmd_base "$@"
+)
---
+++ tests/jenkins/run_nrntraub.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+set -e
+source ${JENKINS_DIR:-.}/_env_setup.sh
+source ${JENKINS_DIR:-.}/_test_utils.sh
+
+set -x
+
+CORENRN_TYPE="$1"
+MPI_RANKS="$2"
+THREADS="$3"
+
+cd $WORKSPACE/nrntraub
+
+# Allocate 1 node
+N=1
+
+bb5_run ./x86_64/special -mpi -c mytstop=100 -c use_coreneuron=0 init.hoc
+
+export OMP_NUM_THREADS=${THREADS}
+n=${MPI_RANKS} bb5_run ./${CORENRN_TYPE}/special-core --mpi -d coredat
+
+sort -n -k'1,1' -k2 < out.dat | awk 'NR==1 { print; next } { printf "%.3f\t%d\n", $1, $2 }' > out_cn.sorted
+sort -n -k'1,1' -k2 < out36.dat | awk 'NR==1 { print; next } { printf "%.3f\t%d\n", $1, $2 }' > out_n.sorted
+
+sdiff -s out_cn.sorted out_n.sorted

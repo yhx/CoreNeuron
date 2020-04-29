86343c7218a69fa3b1317f9ddfb5e77b906f060d
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-04-29 11:51:33 +0200

    Added --voltage 1000 in corenrn to get same results

--- tests/jenkins/run_nrntraub.sh
+++ tests/jenkins/run_nrntraub.sh
@@ -19,9 +19,9 @@ bb5_run ./x86_64/special -mpi -c mytstop=100 -c use_coreneuron=0 init.hoc
 
 if [ "${CORENRN_TYPE}" = "SoA" ]; then
   export OMP_NUM_THREADS=${THREADS}
-  n=${MPI_RANKS} bb5_run ./${CORENRN_TYPE}/special-core --mpi -d coredat
+  n=${MPI_RANKS} bb5_run ./${CORENRN_TYPE}/special-core --mpi -d coredat --voltage=1000
 elif [ "${CORENRN_TYPE}" = "AoS" ]; then
-  bb5_run ./${CORENRN_TYPE}/special-core --mpi -d coredat
+  bb5_run ./${CORENRN_TYPE}/special-core --mpi -d coredat --voltage=1000
 fi
 
 sort -n -k'1,1' -k2 < out.dat | awk 'NR==1 { print; next } { printf "%.3f\t%d\n", $1, $2 }' > out_cn.sorted

8cedc1c4db2a09285ea50482df333ab7fc3412ec
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-04-29 19:15:56 +0200

    Set proper number of threads and ranks to run SoA and AoS builds

--- tests/jenkins/run_nrntraub.sh
+++ tests/jenkins/run_nrntraub.sh
@@ -15,15 +15,9 @@ cd $WORKSPACE/nrntraub
 # Allocate 1 node
 N=1
 
-if [ "${CORENRN_TYPE}" = "SoA" ]; then
-  export OMP_NUM_THREADS=${THREADS}
-  n=${MPI_RANKS} bb5_run ./${CORENRN_TYPE}/special-core --mpi -d coredat --voltage=1000
-elif [ "${CORENRN_TYPE}" = "AoS" ]; then
-  bb5_run ./${CORENRN_TYPE}/special-core --mpi -d coredat --voltage=1000
-else
-    echo "Not a valid CORENRN_TYPE"
-    exit 1
-fi
+export OMP_NUM_THREADS=${THREADS}
+
+n=${MPI_RANKS} bb5_run ./${CORENRN_TYPE}/special-core --mpi -d coredat --voltage=1000
 
 sort -n -k'1,1' -k2 < out.dat | awk 'NR==1 { print; next } { printf "%.3f\t%d\n", $1, $2 }' > out_cn_${CORENRN_TYPE}.sorted
 

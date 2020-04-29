e34f5202c13e9adc69e190deb670f69b4d5ce5ef
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-04-29 11:03:32 +0200

    Run nrntraub with corenrn SoA and openmp and AoS pure MPI

--- tests/jenkins/run_nrntraub.sh
+++ tests/jenkins/run_nrntraub.sh
@@ -17,8 +17,12 @@ N=1
 
 bb5_run ./x86_64/special -mpi -c mytstop=100 -c use_coreneuron=0 init.hoc
 
-export OMP_NUM_THREADS=${THREADS}
-n=${MPI_RANKS} bb5_run ./${CORENRN_TYPE}/special-core --mpi -d coredat
+if [ "${CORENRN_TYPE}" = "SoA" ]; then
+  export OMP_NUM_THREADS=${THREADS}
+  n=${MPI_RANKS} bb5_run ./${CORENRN_TYPE}/special-core --mpi -d coredat
+elif [ "${CORENRN_TYPE}" = "AoS" ]; then
+  bb5_run ./${CORENRN_TYPE}/special-core --mpi -d coredat
+fi
 
 sort -n -k'1,1' -k2 < out.dat | awk 'NR==1 { print; next } { printf "%.3f\t%d\n", $1, $2 }' > out_cn.sorted
 sort -n -k'1,1' -k2 < out36.dat | awk 'NR==1 { print; next } { printf "%.3f\t%d\n", $1, $2 }' > out_n.sorted
